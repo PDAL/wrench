4ecb4d73d5214fedf304b44026e5ecab11ab1b2e
Jan Caha <jan.caha@outlook.com>
2025-03-19 13:36:06 +0100

    Adds Python tests (#49)
    * add files and folders
    
    * tests
    
    * tests CI
    
    * update
    
    * more talkative tests
    
    * use laz file
    
    * fix call
    
    * test for clip
    
    ---------
    
    Co-authored-by: Jan Caha <jan.caha@lutraconsulting.co.uk>

---
+++ tests/conftest.py
@@ -0,0 +1,86 @@
+import subprocess
+import typing
+
+import pytest
+import requests
+import utils
+
+
+@pytest.fixture(autouse=True, scope="session")
+def _prepare_data():
+
+    test_data_folder = utils.test_data_folder()
+
+    if not test_data_folder.exists():
+        test_data_folder.mkdir(parents=True)
+
+    base_data = utils.test_data_filepath("stadium-utm.laz")
+
+    if not base_data.exists():
+        # PDAL autzen stadium dataset
+        url = "https://media.githubusercontent.com/media/PDAL/data/refs/heads/main/autzen/stadium-utm.laz"
+
+        r = requests.get(url, timeout=10 * 60)
+
+        with open(base_data, "wb") as f:
+            f.write(r.content)
+            # Run the pdal_wrench command
+
+    files_for_vpc = []
+    for i in range(1, 5):
+
+        clip_gpkg_file = utils.test_data_filepath(f"rectangle{i}.gpkg")
+        clipped_laz_file = utils.test_data_filepath(f"data_clipped{i}.laz")
+
+        files_for_vpc.append(clipped_laz_file)
+
+        if not clipped_laz_file.exists():
+
+            input_file = base_data
+
+            res = subprocess.run(
+                [
+                    utils.pdal_wrench_path(),
+                    "clip",
+                    "--polygon",
+                    str(clip_gpkg_file),
+                    "--output",
+                    str(clipped_laz_file),
+                    "--input",
+                    str(input_file),
+                ],
+                check=True,
+            )
+
+            assert res.returncode == 0
+
+    vpc_file = utils.test_data_filepath("data.vpc")
+
+    if not vpc_file.exists():
+        res = subprocess.run(
+            [
+                utils.pdal_wrench_path(),
+                "build_vpc",
+                "--output",
+                vpc_file.as_posix(),
+                *[f.as_posix() for f in files_for_vpc],
+            ],
+            check=True,
+        )
+
+        assert res.returncode == 0
+
+
+@pytest.fixture
+def laz_files() -> typing.List[str]:
+    """Return a list of laz files"""
+    files = []
+    for i in range(1, 5):
+        files.append(utils.test_data_filepath(f"data_clipped{i}.laz").as_posix())
+    return files
+
+
+@pytest.fixture
+def main_laz_file() -> str:
+    "Return path to the main laz file"
+    return utils.test_data_filepath("stadium-utm.laz").as_posix()
---
+++ tests/test_merge.py
@@ -0,0 +1,44 @@
+import subprocess
+import typing
+
+import numpy as np
+import pdal
+import utils
+
+
+def test_merge_las(laz_files: typing.List[str]):
+    """Test merge las function"""
+
+    merged_las_file = utils.test_data_filepath("data_merged.las")
+
+    res = subprocess.run(
+        [
+            utils.pdal_wrench_path(),
+            "merge",
+            f"--output={merged_las_file.as_posix()}",
+            *laz_files,
+        ],
+        check=True,
+    )
+
+    assert res.returncode == 0
+
+    pipeline = pdal.Reader.las(filename=merged_las_file.as_posix()).pipeline()
+
+    number_of_points = pipeline.execute()
+
+    assert number_of_points == 338163
+
+    # points as numpy array
+    array = pipeline.arrays[0]
+
+    # all points
+    assert isinstance(array, np.ndarray)
+
+    # first point
+    assert isinstance(array[0], np.void)
+    assert len(array[0]) == 20
+    assert isinstance(array[0]["X"], np.float64)
+    assert isinstance(array[0]["Y"], np.float64)
+    assert isinstance(array[0]["Z"], np.float64)
+    assert isinstance(array[0]["Intensity"], np.uint16)
