af6937c7c2456e6f6fc9cca63b06f64ab3309de9
Jan Caha <jan.caha@lutraconsulting.co.uk>
2025-03-19 14:11:19 +0100

    add test for vpc merge

--- tests/test_merge.py
+++ tests/test_merge.py
@@ -42,3 +42,41 @@ def test_merge_las(laz_files: typing.List[str]):
     assert isinstance(array[0]["Y"], np.float64)
     assert isinstance(array[0]["Z"], np.float64)
     assert isinstance(array[0]["Intensity"], np.uint16)
+
+
+def test_merge_vpc(vpc_file: str):
+    """Test merge of vpc file"""
+
+    merged_las_file = utils.test_data_filepath("data_merged.las")
+
+    res = subprocess.run(
+        [
+            utils.pdal_wrench_path(),
+            "merge",
+            f"--output={merged_las_file.as_posix()}",
+            vpc_file,
+        ],
+        check=True,
+    )
+
+    assert res.returncode == 0
+
+    pipeline = pdal.Reader.las(filename=merged_las_file.as_posix()).pipeline()
+
+    number_of_points = pipeline.execute()
+
+    assert number_of_points == 338163
+
+    # points as numpy array
+    array = pipeline.arrays[0]
+
+    # all points
+    assert isinstance(array, np.ndarray)
+
+    # first point
+    assert isinstance(array[0], np.void)
+    assert len(array[0]) == 20
+    assert isinstance(array[0]["X"], np.float64)
+    assert isinstance(array[0]["Y"], np.float64)
+    assert isinstance(array[0]["Z"], np.float64)
+    assert isinstance(array[0]["Intensity"], np.uint16)
