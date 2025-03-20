acf9bc149ed9b39b7d19453dda5571f7bfb50b1e
Jan Caha <jan.caha@lutraconsulting.co.uk>
2025-03-20 13:34:43 +0100

    simplify

--- src/to_vector.cpp
+++ src/to_vector.cpp
@@ -157,8 +157,7 @@ void ToVector::finalize(std::vector<std::unique_ptr<PipelineManager>>&)
 
             for (const std::string& f : tileOutputFiles)
             {   
-                std::string layerName = fs::path(f).stem().string();
-                outputStreamFile << "<OGRVRTLayer name=\"" << layerName << "\">" << std::endl;
+                outputStreamFile << "<OGRVRTLayer name=\"" << fs::path(f).stem().string() << "\">" << std::endl;
                 outputStreamFile << "<SrcDataSource>" << f <<  "</SrcDataSource>" << std::endl;
                 outputStreamFile << "<SrcLayer>points</SrcLayer>" << std::endl;
                 outputStreamFile << "</OGRVRTLayer>" << std::endl;
