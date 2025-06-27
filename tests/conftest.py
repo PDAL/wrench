d27b9d8dbf39f94bc01448c02f90befbe2172635
Jan Caha <jan.caha@lutraconsulting.co.uk>
2025-06-27 14:19:40 +0200

    create vpc with copc files

--- tests/conftest.py
+++ tests/conftest.py
@@ -33,12 +33,16 @@ def _prepare_data():
     assert number_points == 693895
 
     files_for_vpc = []
+    files_for_vpc_copc = []
+    
     for i in range(1, 5):
 
         clip_gpkg_file = utils.test_data_filepath(f"rectangle{i}.gpkg")
         clipped_laz_file = utils.test_data_filepath(f"data_clipped{i}.laz")
+        clipped_copc_file = utils.test_data_filepath(f"data_clipped{i}.copc.laz")
 
         files_for_vpc.append(clipped_laz_file)
+        files_for_vpc_copc.append(clipped_copc_file)
 
         if not clipped_laz_file.exists():
 
@@ -67,6 +71,31 @@ def _prepare_data():
 
         assert clipped_laz_file.exists()
 
+        if not clipped_copc_file.exists():
+
+            input_file = base_data
+
+            res = subprocess.run(
+                [
+                    utils.pdal_wrench_path(),
+                    "clip",
+                    "--polygon",
+                    str(clip_gpkg_file),
+                    "--output",
+                    str(clipped_copc_file),
+                    "--input",
+                    str(input_file),
+                ],
+                check=True,
+            )
+
+            assert res.returncode == 0
+
+            clipped_copc = pdal.Reader(clipped_copc_file.as_posix()).pipeline()
+            number_points = clipped_copc.execute()
+
+            assert number_points > 0
+
     vpc_file = utils.test_data_filepath("data.vpc")
 
     if not vpc_file.exists():
@@ -87,6 +116,29 @@ def _prepare_data():
 
     vpc = pdal.Reader(vpc_file.as_posix()).pipeline()
     number_points = vpc.execute()
+
+    assert number_points == 338163
+
+    vpc_copc_file = utils.test_data_filepath("data_copc.vpc")
+
+    if not vpc_copc_file.exists():
+        res = subprocess.run(
+            [
+                utils.pdal_wrench_path(),
+                "build_vpc",
+                "--output",
+                vpc_copc_file.as_posix(),
+                *[f.as_posix() for f in files_for_vpc_copc],
+            ],
+            check=True,
+        )
+
+        assert res.returncode == 0
+
+    assert vpc_file.exists()
+
+    vpc_copc = pdal.Reader(vpc_copc_file.as_posix()).pipeline()
+    number_points = vpc_copc.execute()
 
     assert number_points == 338163
 
@@ -132,6 +184,11 @@ def main_laz_file() -> str:
 def vpc_file() -> str:
     "Return path to the vpc file"
     return utils.test_data_filepath("data.vpc").as_posix()
+
+@pytest.fixture
+def vpc_copc_file() -> str:
+    "Return path to the vpc file"
+    return utils.test_data_filepath("data_copc.vpc").as_posix()
 
 
 @pytest.fixture
