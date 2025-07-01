c31b72a5f791d16478103818b9e26fd269e8c847
Jan Caha <jan.caha@lutraconsulting.co.uk>
2025-07-01 13:27:10 +0200

    refactor so that input and output are paths

--- tests/test_translate.py
+++ tests/test_translate.py
@@ -7,53 +7,34 @@ import utils
 
 
 @pytest.mark.parametrize(
-    "input_type,output,point_count",
+    "input_path,output_path,point_count",
     [
-        ("main_laz", utils.test_data_filepath("translate.las"), 693895),
-        ("main_laz", utils.test_data_filepath("translate.copc.laz"), 693895),
-        ("main_copc", utils.test_data_filepath("translate-copc-input.laz"), 693895),
-        ("vpc_file", utils.test_data_filepath("translate-vpc.copc.laz"), 338163),
-        ("vpc_copc_file", utils.test_data_filepath("translate.vpc"), 338163),
+        (utils.test_data_filepath("stadium-utm.laz"), utils.test_data_filepath("translate.las"), 693895),
+        (utils.test_data_filepath("stadium-utm.laz"), utils.test_data_filepath("translate.copc.laz"), 693895),
+        (utils.test_data_filepath("stadium-utm.copc.laz"), 693895),
+        (utils.test_data_filepath("data.vpc"), utils.test_data_filepath("translate-vpc.copc.laz"), 338163),
+        (utils.test_data_filepath("data_copc.vpc"), utils.test_data_filepath("translate.vpc"), 338163),
     ],
 )
-def test_translate_files(
-    input_type: str,
-    output: Path,
-    point_count: int,
-    main_laz_file: str,
-    main_copc_file: str,
-    vpc_laz_file: str,
-    vpc_copc_file: str,
-):
+def test_translate_files(input_path: Path, output_path: Path, point_count: int):
     """Test translate las function"""
 
-    if input_type == "main_laz":
-        input = main_laz_file
-    elif input_type == "main_copc":
-        input = main_copc_file
-    elif input_type == "vpc_file":
-        input = vpc_laz_file
-    elif input_type == "vpc_copc_file":
-        input = vpc_copc_file
-    else:
-        assert False, "Invalid input type"
-
-    print(f"{utils.pdal_wrench_path()} translate --input={input} --output={output.as_posix()}")
+    print(f"{utils.pdal_wrench_path()} translate --input={input_path} --output={output_path.as_posix()}")
     res = subprocess.run(
         [
             utils.pdal_wrench_path(),
             "translate",
-            f"--input={input}",
-            f"--output={output.as_posix()}",
+            f"--input={input_path.as_posix()}",
+            f"--output={output_path.as_posix()}",
         ],
         check=True,
     )
 
     assert res.returncode == 0
 
-    assert output.exists()
+    assert output_path.exists()
 
-    pipeline = pdal.Reader(filename=output.as_posix()).pipeline()
+    pipeline = pdal.Reader(filename=output_path.as_posix()).pipeline()
 
     number_of_points = pipeline.execute()
 
