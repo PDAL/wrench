3d666fa370cfe06e168375e9d92648caf55dac00
Jan Caha <jan.caha@outlook.com>
2025-11-28 10:13:01 +0100

    update test paths

--- tests/test_translate.py
+++ tests/test_translate.py
@@ -9,21 +9,36 @@ import utils
 @pytest.mark.parametrize(
     "input_path,output_path,point_count",
     [
-        (utils.test_data_filepath("stadium-utm.laz"), utils.test_data_filepath("translate.las"), 693895),
-        (utils.test_data_filepath("stadium-utm.laz"), utils.test_data_filepath("translate.copc.laz"), 693895),
         (
-            utils.test_data_filepath("stadium-utm.copc.laz"),
-            utils.test_data_filepath("translate-copc-input.copc.laz"),
+            utils.test_data_filepath("stadium-utm.laz"),
+            utils.test_data_output_filepath("translate.las", "translate"),
             693895,
         ),
-        (utils.test_data_filepath("data.vpc"), utils.test_data_filepath("translate-vpc.copc.laz"), 338163),
-        (utils.test_data_filepath("data_copc.vpc"), utils.test_data_filepath("translate.vpc"), 338163),
+        (
+            utils.test_data_filepath("stadium-utm.laz"),
+            utils.test_data_output_filepath("translate.copc.laz", "translate"),
+            693895,
+        ),
+        (
+            utils.test_data_filepath("stadium-utm.copc.laz"),
+            utils.test_data_output_filepath("translate-copc-input.copc.laz", "translate"),
+            693895,
+        ),
+        (
+            utils.test_data_filepath("data.vpc"),
+            utils.test_data_output_filepath("translate-vpc.copc.laz", "translate"),
+            338163,
+        ),
+        (
+            utils.test_data_filepath("data_copc.vpc"),
+            utils.test_data_output_filepath("translate.vpc", "translate"),
+            338163,
+        ),
     ],
 )
 def test_translate_files(input_path: Path, output_path: Path, point_count: int):
     """Test translate function"""
 
-    print(f"{utils.pdal_wrench_path()} translate --input={input_path} --output={output_path.as_posix()}")
     res = subprocess.run(
         [
             utils.pdal_wrench_path(),
