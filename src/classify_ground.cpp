a69f21d82ee562396fb60339e1f69a24d77bd693
Jan Caha <jan.caha@outlook.com>
2025-12-05 11:44:19 +0100

    update to new utility functions

--- src/classify_ground.cpp
+++ src/classify_ground.cpp
@@ -70,9 +70,7 @@ static std::unique_ptr<PipelineManager> pipeline(ParallelJobInfo *tile, pdal::Op
 {
     std::unique_ptr<PipelineManager> manager( new PipelineManager );
 
-    Options reader_opts;
-
-    Stage& r = manager->makeReader( tile->inputFilenames[0], "", reader_opts);
+    Stage& r = makeReader(manager.get(), tile->inputFilenames[0]);
 
     Stage *last = &r;
 
@@ -102,13 +100,9 @@ static std::unique_ptr<PipelineManager> pipeline(ParallelJobInfo *tile, pdal::Op
     }
 
     last = &manager->makeFilter( "filters.smrf", *last, filterOptions);
+ 
+    makeWriter(manager.get(), tile->outputFilename, last);
 
-    pdal::Options writer_opts;
-    // let's use the same offset & scale & header & vlrs as the input
-    writer_opts.add(pdal::Option("forward", "all"));
-       
-    (void)manager->makeWriter( tile->outputFilename, "", *last, writer_opts);
-        
     return manager;
 }
 
@@ -169,29 +163,5 @@ void ClassifyGround::finalize(std::vector<std::unique_ptr<PipelineManager>>&)
     if (tileOutputFiles.empty())
         return;
 
-    std::vector<std::string> args;
-    args.push_back("--output=" + outputFile);
-    for (std::string f : tileOutputFiles)
-        args.push_back(f);
-
-    if (ends_with(outputFile, ".vpc"))
-    {
-        // now build a new output VPC
-        buildVpc(args);
-    }
-    else
-    {
-        // merge all the output files into a single file        
-        Merge merge;
-        // for copc set isStreaming to false
-        if (ends_with(outputFile, ".copc.laz"))
-        {
-            merge.isStreaming = false;
-        }
-
-        runAlg(args, merge);
-
-        // remove files as they are not needed anymore - they are merged
-        removeFiles(tileOutputFiles, true);
-    }
+    buildOutput(outputFile, tileOutputFiles);
 }
