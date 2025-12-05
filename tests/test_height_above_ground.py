da6fb5ddb56a580082606c7cda018a8e4509c533
Jan Caha <jan.caha@outlook.com>
2025-12-05 15:05:43 +0100

    simplify tests

--- tests/test_height_above_ground.py
+++ tests/test_height_above_ground.py
@@ -6,57 +6,12 @@ import pdal
 import pytest
 import utils
 
-# Base test cases with input, output, and expected point count
-test_cases = [
-    (
-        utils.test_data_filepath("stadium-utm.laz"),
-        utils.test_data_output_filepath("stadium-utm.las", "height_above_ground"),
-        693895,
-    ),
-    (
-        utils.test_data_filepath("stadium-utm.laz"),
-        utils.test_data_output_filepath("stadium-utm.copc.laz", "height_above_ground"),
-        693895,
-    ),
-    (
-        utils.test_data_filepath("stadium-utm.copc.laz"),
-        utils.test_data_output_filepath("stadium-utm-copc-input.copc.laz", "height_above_ground"),
-        693895,
-    ),
-    (
-        utils.test_data_filepath("data.vpc"),
-        utils.test_data_output_filepath("stadium-utm-vpc.copc.laz", "height_above_ground"),
-        338163,
-    ),
-    (
-        utils.test_data_filepath("data_copc.vpc"),
-        utils.test_data_output_filepath("stadium-utm-vpc-copc.vpc", "height_above_ground"),
-        338163,
-    ),
-]
 
-# algorithms
-algorithms = ["nn", "delaunay"]
+def test_hag_replace_z():
+    """Test height above ground with replace Z option."""
 
-# main setting of the height_above_ground
-replace_z_values = [True, False]
-
-# Create all combinations for tests
-parametrize_values = []
-for (input_path, output_path, point_count), algorithm, replace_z in itertools.product(
-    test_cases, algorithms, replace_z_values
-):
-    parametrize_values.append((input_path, output_path, point_count, replace_z, algorithm))
-
-
-@pytest.mark.parametrize(
-    "input_path,output_path,point_count,replace_z,algorithm",
-    parametrize_values,
-)
-def test_height_above_ground_files(
-    input_path: Path, output_path: Path, point_count: int, replace_z: bool, algorithm: str
-):
-    """Test height_above_ground function"""
+    input_path = utils.test_data_filepath("stadium-utm.laz")
+    output_path = utils.test_data_output_filepath("stadium-utm.las", "height_above_ground")
 
     res = subprocess.run(
         [
@@ -64,8 +19,48 @@ def test_height_above_ground_files(
             "height_above_ground",
             f"--input={input_path.as_posix()}",
             f"--output={output_path.as_posix()}",
-            f"--algorithm={algorithm}",
-            f"--replace-z={str(replace_z).lower()}",
+            f"--algorithm=nn",
+            f"--replace-z=true",
+        ],
+    )
+
+    assert res.returncode == 0
+
+    assert output_path.exists()
+
+    if not output_path.name.endswith(".copc.laz") and output_path.suffix.lower() in [".laz", ".las"]:
+        pipeline = pdal.Reader(filename=output_path.as_posix(), use_eb_vlr=True).pipeline()
+    else:
+        pipeline = pdal.Reader(filename=output_path.as_posix()).pipeline()
+
+    number_of_points = pipeline.execute()
+
+    assert number_of_points == 693895
+
+    dimensions = pipeline.arrays[0].dtype.names
+
+    # check dimensions
+    assert "HeightAboveGround" in dimensions
+
+    values = pipeline.arrays[0]["Z"]
+    assert min(values) == pytest.approx(-0.85, abs=0.01)
+    assert max(values) == pytest.approx(43.82, abs=0.01)
+
+
+def test_hag_heightaboveground_dim():
+    """Test height above ground with using the HeightAboveGround dimension."""
+
+    input_path = utils.test_data_filepath("stadium-utm.laz")
+    output_path = utils.test_data_output_filepath("stadium-utm.copc.laz", "height_above_ground")
+
+    res = subprocess.run(
+        [
+            utils.pdal_wrench_path(),
+            "height_above_ground",
+            f"--input={input_path.as_posix()}",
+            f"--output={output_path.as_posix()}",
+            f"--algorithm=nn",
+            f"--replace-z=false",
         ],
     )
 
@@ -85,21 +80,14 @@ def test_height_above_ground_files(
     # check dimensions
     assert "HeightAboveGround" in dimensions
 
-    # for non vpc files check values (vpc have different values)
-    if input_path.suffix.lower() != ".vpc" and algorithm == "nn":
-        if replace_z:
-            values = pipeline.arrays[0]["Z"]
-            assert min(values) == pytest.approx(-0.85, abs=0.01)
-            assert max(values) == pytest.approx(43.82, abs=0.01)
-        else:
-            values = pipeline.arrays[0]["Z"]
-            assert min(values) == pytest.approx(126.64, abs=0.01)
-            assert max(values) == pytest.approx(182.32, abs=0.01)
-            values = pipeline.arrays[0]["HeightAboveGround"]
-            assert min(values) == pytest.approx(-0.85, abs=0.01)
-            assert max(values) == pytest.approx(43.82, abs=0.01)
+    values = pipeline.arrays[0]["Z"]
+    assert min(values) == pytest.approx(126.64, abs=0.01)
+    assert max(values) == pytest.approx(182.32, abs=0.01)
+    values = pipeline.arrays[0]["HeightAboveGround"]
+    assert min(values) == pytest.approx(-0.85, abs=0.01)
+    assert max(values) == pytest.approx(43.82, abs=0.01)
 
-    assert number_of_points == point_count
+    assert number_of_points == 693895
 
 
 def test_run_with_bad_algorithms():
