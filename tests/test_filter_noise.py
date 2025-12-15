ed664d3177a302d8dea4aa1a8ebe65922bf39fc3
Jan Caha <jan.caha@outlook.com>
2025-12-15 11:34:13 +0100

    remove noise points

--- tests/test_filter_noise.py
+++ tests/test_filter_noise.py
@@ -136,3 +136,49 @@ def test_filter_noise_with_bad_algorithms():
 
     assert res.returncode == 0
     assert f"unknown algorithm: {unknown_algorithm}" in res.stderr
+
+
+def test_filter_noise_statistical_remove_noise_points():
+    """Test filter noise function with statistical on laz and removal of noise points"""
+
+    input_path = utils.test_data_filepath("stadium-utm.laz")
+    output_path = utils.test_data_output_filepath("filter_noise_remove_points.laz", "filter_noise")
+
+    res = subprocess.run(
+        [
+            utils.pdal_wrench_path(),
+            "filter_noise",
+            f"--input={input_path.as_posix()}",
+            f"--output={output_path.as_posix()}",
+            "--algorithm=statistical",
+            "--remove-noise-points=true",
+        ],
+        check=True,
+    )
+
+    assert res.returncode == 0
+
+    assert output_path.exists()
+
+    pipeline = pdal.Reader(filename=output_path.as_posix()).pipeline()
+
+    pipeline.execute()
+
+    values = pipeline.arrays[0]["Classification"]
+    unique_values = np.unique(values)
+
+    assert unique_values.size == 2
+    assert unique_values.tolist() == [1, 2]  # 1: non-ground, 2: ground
+
+    nonground_point = pipeline.arrays[0][
+        (pipeline.arrays[0]["X"] == 494629.19000000006) & (pipeline.arrays[0]["Y"] == 4878441.75)
+    ]
+    assert nonground_point["Classification"] == 1  # non-ground
+    assert nonground_point["Z"] == 129.14
+
+    ground_point = pipeline.arrays[0][(pipeline.arrays[0]["X"] == 494657.37) & (pipeline.arrays[0]["Y"] == 4878358.05)]
+    assert ground_point["Classification"] == 2  # ground
+    assert ground_point["Z"] == 130.15999999999997
+
+    noise_point = pipeline.arrays[0][(pipeline.arrays[0]["X"] == 494650.53) & (pipeline.arrays[0]["Y"] == 4878439.98)]
+    assert noise_point.size == 0
