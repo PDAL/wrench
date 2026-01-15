2873ebb0f4228b489baddbff803f9896be28da44
Jan Caha <jan.caha@outlook.com>
2026-01-15 12:25:28 +0100

    update version number

--- src/main.cpp
+++ src/main.cpp
@@ -30,7 +30,7 @@ TODO:
 
 extern int runTile(std::vector<std::string> arglist);  // tile/tile.cpp
 
-std::string WRENCH_VERSION = "1.3.1";
+std::string WRENCH_VERSION = "1.4.0";
 
 void printUsage()
 {
