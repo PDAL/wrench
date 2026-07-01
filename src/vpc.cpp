ea38f926fa88554dc58f583ce25775a883ccaee1
Jan Caha <jan.caha@outlook.com>
2026-07-01 09:18:43 +0200

    Add parameter to force use absolute paths while building VPC (#91)
    * bump version
    
    * add parameter to force use of absolute paths
    
    * version
    
    * use version from main
    
    * if relative path ends with empty string do not create invalid VPC, raise error instead suggesting using absolute paths

--- src/vpc.cpp
+++ src/vpc.cpp
@@ -271,7 +271,7 @@ void geometryToJson(const Geometry &geom, const BOX3D &bbox, nlohmann::json &jso
     }
 }
 
-bool VirtualPointCloud::write(std::string filename)
+bool VirtualPointCloud::write(std::string filename, bool forceAbsolutePaths)
 {
     if (!isVpcFilename(filename))
         filename += ".vpz";
@@ -288,15 +288,24 @@ bool VirtualPointCloud::write(std::string filename)
     for ( const File &f : files )
     {
         std::string assetFilename;
-        if (pdal::Utils::isRemote(f.filename))
+        if (pdal::Utils::isRemote(f.filename) || forceAbsolutePaths)
         {
-            // keep remote URLs as they are
+            // keep remote URLs or absolute paths as they are
             assetFilename = f.filename;
         }
         else
         {
             // turn local paths to relative
             fs::path fRelative = fs::relative(f.filename, outputPath);
+
+            if (fRelative.empty()) {
+              std::cerr << "failed to make filename relative to output path: "
+                        << f.filename 
+                        << " consider using --use-absolute-paths"
+                        << std::endl;
+              return false;
+            }
+
             assetFilename = "./" + fRelative.string();
         }
         std::string fileId = fs::path(f.filename).stem().string();  // TODO: we should make sure the ID is unique
@@ -379,9 +388,19 @@ bool VirtualPointCloud::write(std::string filename)
         for (size_t i = 0; i < f.overviewFilenames.size(); ++i)
         {
             std::string ovFilename(f.overviewFilenames[i]);
-            if (!pdal::Utils::isRemote(ovFilename))
+            if (!pdal::Utils::isRemote(ovFilename) && !forceAbsolutePaths)
             {
                 const fs::path fRelative = fs::relative(ovFilename, outputPath);
+                
+                if (fRelative.empty())
+                {
+                  std::cerr << "failed to make overview filename relative to output path: "
+                            << ovFilename
+                            << " consider using --use-absolute-paths"
+                            << std::endl;
+                  return false;
+                }
+
                 ovFilename = "./" + fRelative.string();
             }
             const std::string key = f.overviewFilenames.size() > 1 ? ("overview-" + std::to_string(i + 1)) : "overview";
@@ -526,6 +545,7 @@ void buildVpc(std::vector<std::string> args)
     int max_threads = -1;
     bool verbose = false;
     bool help = false;
+    bool forceAbsolutePaths = false;
 
     ProgramArgs programArgs;
     programArgs.add("help,h", "Output command help.", help);
@@ -541,6 +561,7 @@ void buildVpc(std::vector<std::string> args)
 
     pdal::Arg& argThreads = programArgs.add("threads", "Max number of concurrent threads for parallel runs", max_threads);
     programArgs.add("verbose", "Print extra debugging output", verbose);
+    programArgs.add("use-absolute-paths", "Store absolute file paths instead of relative paths in the output VPC", forceAbsolutePaths);
 
     try
     {
@@ -914,7 +935,7 @@ void buildVpc(std::vector<std::string> args)
         }
     }
 
-    vpc.write(outputFile);
+    vpc.write(outputFile, forceAbsolutePaths);
 
     // TODO: for now hoping that all files have the same file type + CRS + point format + scaling
     // "dataformat_id"
